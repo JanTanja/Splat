ecee9a34c2edef2b99b227f01a7053b1dfd8ffab
Jan Tanja <jtanja@ucmerced.edu>
2019-04-28 00:28:17 -0700

    Front view rough draft

---
+++ Splat/AccountManagement.swift
@@ -0,0 +1,34 @@
+//
+//  AccountManagement.swift
+//  Splat
+//
+//  Created by Jan Ephraim Nino Tanja on 4/28/19.
+//  Copyright © 2019 Jan Ephraim Nino Tanja. All rights reserved.
+//
+
+import Foundation
+
+
+class DataAcquisition {
+    
+    var users:[User] = []
+    var chats:[Chat] = []
+    static func createAccount() {
+        // perform checks
+        // add to users
+        
+    }
+}
+class LoginHandler {
+    static func checkUserNameExists(input: String) -> Bool {
+        // let's check if their username is in the database!
+        if input.isEmpty {return false}
+        let userNameExists: Bool = true
+        return userNameExists
+    }
+    static func checkPasswordExists(input: String) -> Bool {
+        if input.isEmpty {return false}
+        let passwordExists: Bool = true
+        return passwordExists
+    }
+}
--- Splat/ViewController.swift
+++ Splat/ViewController.swift
@@ -10,19 +10,38 @@ import UIKit
 import FacebookCore
 import FacebookLogin
 
+
+
 class ViewController: UIViewController {
 
     // This app will be called Splat :)
-    var users:[User] = []
-    var chats:[Chat] = []
+    @IBOutlet weak var userNameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var firstUser = User(userName: "username", password: "password")
+        
+        // log-in
+        var _ = User(userName: "username", password: "password")
         let loginButton = LoginButton(readPermissions: [.publicProfile])
         loginButton.center = view.center
+        loginButton.frame = CGRect(x: loginButton.center.x - (loginButton.frame.size.width / 2), y: loginButton.center.y + 100, width: loginButton.frame.size.width, height: loginButton.frame.size.height)
+        
         self.view.addSubview(loginButton)
         
+    }
+    @IBAction func pressedLoginButton(_ sender: UIButton) {
+        
+        if (LoginHandler.checkUserNameExists(input: self.userNameInput.text!) && LoginHandler.checkPasswordExists(input: self.passwordInput.text!)) {
+            // you can do segue
+            self.performSegue(withIdentifier: "loginSuccessfulSegue", sender: self)
+            print("Correct login! :)")
+        }
+        else {
+            // render a message saying that you cannot proceed
+            print("Incorrect username or password -- please try again!")
+        }
     }
     
 }
